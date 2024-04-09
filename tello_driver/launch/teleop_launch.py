ca8e4c4ecf4d09214d4aef64e330424f8c7d3334
djwithey <97760378+djwithey@users.noreply.github.com>
2024-04-09 18:46:31 +0100

    Update teleop_launch.py with camera_info.yaml path

--- tello_driver/launch/teleop_launch.py
+++ tello_driver/launch/teleop_launch.py
@@ -1,13 +1,19 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
 
 # Launch nodes required for joystick operation
 
-
 def generate_launch_description():
+    camera_file = os.path.join(
+            get_package_share_directory('tello_driver'),
+            'cfg',
+            'camera_info.yaml')
     return LaunchDescription([
         Node(package='joy', executable='joy_node', output='screen'),
         Node(package='tello_driver', executable='tello_joy_main', output='screen'),
-        Node(package='tello_driver', executable='tello_driver_main', output='screen'),
+        Node(package='tello_driver', executable='tello_driver_main', output='screen',
+             parameters=[{'camera_info_path': camera_file}],
+            ),
     ])
